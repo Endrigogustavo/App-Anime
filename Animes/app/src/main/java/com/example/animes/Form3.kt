05510b089f9d8d843be2b34edcf5e3b25fb223db
/__Endrigo__ <113954684+Endrigogustavo@users.noreply.github.com>
2023-09-06 14:28:53 -0300

    a

---
+++ Animes/app/src/main/java/com/example/animes/Form3.kt
@@ -0,0 +1,306 @@
+package com.example.animes
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.animes.ui.theme.AnimesTheme
+
+
+
+
+@Composable
+fun App(){
+    AnimesTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Box(
+                modifier = with (Modifier){
+                    fillMaxSize()
+                        .background(Color(0xFF070D24))
+
+                })
+            {
+                // Add more views here!
+                FormLayoutFilled()
+            }
+
+
+
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun FormLayoutFilled() {
+    val focusManager = LocalFocusManager.current
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .widthIn(max = 480.dp)
+                .padding(top = 40.dp)
+                .padding(30.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(vertical = 24.dp)
+        ) {
+            item {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    Box(
+                        modifier = Modifier.size(156.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+                    }
+
+                }
+            }
+            item {
+                var imageUrl = remember { mutableStateOf<String?>(null) }
+                Box(Modifier.fillMaxWidth()) {
+                }
+            }
+            item {
+                var text by remember { mutableStateOf("") }
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    label = { Text("Nome") },
+                    value = text,
+                    onValueChange = { text = it },
+                    singleLine = true,
+                    trailingIcon = {
+                        AnimatedVisibility(
+                            visible = text.isNotBlank(),
+                            enter = fadeIn(),
+                            exit = fadeOut()
+                        ) {
+                            IconButton(onClick = { text = "" }) {
+                                Icon(Icons.Outlined.Edit, "Clear")
+                            }
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Next,
+                        capitalization = KeyboardCapitalization.Words
+                    ),
+                    keyboardActions = KeyboardActions {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                )
+
+            }
+            item {
+                var text by remember { mutableStateOf("") }
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    label = { Text("Telefone") },
+                    value = text,
+                    onValueChange = { text = it },
+                    singleLine = true,
+                    trailingIcon = {
+                        AnimatedVisibility(
+                            visible = text.isNotBlank(),
+                            enter = fadeIn(),
+                            exit = fadeOut()
+                        ) {
+                            IconButton(onClick = { text = "" }) {
+                                Icon(Icons.Outlined.Edit, "Clear")
+                            }
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Next,
+                        capitalization = KeyboardCapitalization.Words
+                    ),
+                    keyboardActions = KeyboardActions {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                )
+            }
+            item { Spacer(Modifier.height(4.dp)) }
+            item {
+                val options = listOf("Telefone Fixo", "Celular", "Whatsapp")
+                var expanded by remember { mutableStateOf(false) }
+                var selectedOptionText by remember { mutableStateOf("") }
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = { expanded = !expanded },
+                ) {
+                    TextField(
+                        // The `menuAnchor` modifier must be passed to the text field for correctness.
+                        modifier = Modifier.menuAnchor()
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        value = selectedOptionText,
+                        onValueChange = { selectedOptionText = it },
+                        label = { Text("Tipo de Telefone") },
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                        colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                    )
+                    // filter options based on text field value
+                    val filteringOptions = options.filter { it.contains(selectedOptionText, ignoreCase = true) }
+                    if (filteringOptions.isNotEmpty()) {
+                        ExposedDropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false },
+                        ) {
+                            filteringOptions.forEach { selectionOption ->
+                                DropdownMenuItem(
+                                    text = { Text(selectionOption) },
+                                    onClick = {
+                                        selectedOptionText = selectionOption
+                                        expanded = false
+                                    },
+                                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            item { Spacer(Modifier.height(4.dp)) }
+            item {
+                var text by remember { mutableStateOf("") }
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    label = { Text("Observação") },
+                    value = text,
+                    onValueChange = { text = it },
+                    trailingIcon = {
+                        AnimatedVisibility(
+                            visible = text.isNotBlank(),
+                            enter = fadeIn(),
+                            exit = fadeOut()
+                        ) {
+                            IconButton(onClick = { text = "" }) {
+                                Icon(Icons.Outlined.Edit, "Clear")
+                            }
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions {
+                        focusManager.clearFocus()
+                    },
+                    singleLine = true,
+                )
+            }
+
+            item {
+                var selected by remember { mutableStateOf(false) }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(24.dp),
+                    modifier = Modifier
+                        .clickable { selected = selected.not() }
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+
+                    ){
+
+                        Button(
+                            // border = BorderStroke(width = 2.dp, color = Color(0xFFAC6AB4)),
+                            onClick = { /* Do something! */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(50.dp)
+                                .padding(horizontal = 16.dp),
+                            colors = ButtonDefaults.buttonColors(Color(0xFF5369D6))
+                        ) {
+                            Icon(
+                                Icons.Filled.Add,
+                                contentDescription = "Localized description",
+                                modifier = Modifier.size(ButtonDefaults.IconSize)
+                            )
+                            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                            Text("Cadastrar")
+                        }
+                        Button(
+                            // border = BorderStroke(width = 2.dp, color = Color(0xFFAC6AB4)),
+                            onClick = { /* Do something! */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(50.dp)
+                                .padding(horizontal = 16.dp),
+                            colors = ButtonDefaults.buttonColors(Color(0xFF1C37BE))
+                        ) {
+                            Icon(
+                                Icons.Filled.Clear,
+                                contentDescription = "Localized description",
+                                modifier = Modifier.size(ButtonDefaults.IconSize)
+                            )
+                            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                            Text("Cancelar")
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+}
+
+
+
